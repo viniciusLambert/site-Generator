ca5d3b162056925c6eff0039d24d4876c975dc99
vinicius.lambert <vinicius.lambert@proton.me>
2026-02-08 07:30:36 -0300

    Add: block type

---
+++ src/domain/block_type.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph text" 
+    HEADING = "heading text"
+    CODE = "code text"
+    QUOTE = "quote text"
+    UNORDERED_LIST = "unordered_list text"
+    ORDERED_LIST = "ordered_list text"
+
