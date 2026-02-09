7a9d5d05c953a2b75bcfed34e7050cb80a3e67e3
vinicius.lambert <vinicius.lambert@proton.me>
2026-02-09 11:32:48 -0300

    fix: reference the right path to create a static site for github

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 PYTHONPATH=src 
 
 python3 src/main.py
-cd public && python3 -m http.server 8888
+cd docs && python3 -m http.server 8888
--- src/main.py
+++ src/main.py
@@ -5,8 +5,9 @@ from usecases.generator.generate_pages import generate_pages_recursive
 def main():
     base_path = "/" 
     if len(sys.argv) > 1:
-        base_path= sys.argv[0]
+        base_path= sys.argv[1]
 
+    print(base_path)
     copy_directory("static", "docs")        
     generate_pages_recursive("content", "template.html", "docs", base_path)
 
